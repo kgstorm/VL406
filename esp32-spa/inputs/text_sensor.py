0c7e8249475d7fcfc3a807b26208fc1b4c08b36f
Kevin <kevin.storm@gmail.com>
2026-08-30 09:43:37 +0100

    Port filter cycle support to VL406

--- esp32-spa/inputs/text_sensor.py
+++ esp32-spa/inputs/text_sensor.py
@@ -11,16 +11,22 @@ CONF_PARENT_ID = 'parent_id'
 # This platform requires referencing an existing HotTubDisplaySensor instance
 CONFIG_SCHEMA = text_sensor.text_sensor_schema().extend({
     cv.Required(CONF_PARENT_ID): cv.use_id(HotTubDisplaySensor),
-    cv.Required('type'): cv.enum({'error_code': 'ERROR_CODE', 'spa_mode': 'SPA_MODE'}),
+    cv.Required('type'): cv.enum({
+        'error_code': 'ERROR_CODE',
+        'spa_mode': 'SPA_MODE',
+        'filter_cycle': 'FILTER_CYCLE'
+    }),
 })
 
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_PARENT_ID])
     var = await text_sensor.new_text_sensor(config)
-    
+
     sensor_type = config['type']
     if sensor_type == 'error_code':
         cg.add(parent.set_error_text_sensor(var))
     elif sensor_type == 'spa_mode':
         cg.add(parent.set_spa_mode_text_sensor(var))
+    elif sensor_type == 'filter_cycle':
+        cg.add(parent.set_filter_cycle_text_sensor(var))
